b71e7f2bbff80e7f42da1d0a67488ec473b09966
platypus3036 <akakehcn@gmail.com>
2025-02-21 14:53:03 +0900

    feat:connect kis open api and server with websocket

---
+++ src/main/java/org/scoula/backend/order/service/kiswebsocket/StockData.java
@@ -0,0 +1,21 @@
+package org.scoula.backend.order.service.kiswebsocket;
+
+import lombok.Data;
+
+@Data
+public class StockData {
+	private String time;          // 체결 시각
+	private double currentPrice;  // 현재가
+	private double openPrice;     // 시가
+	private double highPrice;     // 고가
+	private double lowPrice;      // 저가
+	private double volume;        // 체결량
+	private double accVolume;     // 누적거래량
+	private String changeSign;    // 전일대비구분
+	private double changePrice;   // 전일대비
+	private double changeRate;    // 등락률
+	private double askPrice;      // 매도호가
+	private double bidPrice;      // 매수호가
+	private long askVolume;       // 매도잔량
+	private long bidVolume;       // 매수잔량
+}
