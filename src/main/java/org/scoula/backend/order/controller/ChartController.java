989a731b34309af1b6f2c4c975ef245796ff7181
platypus3036 <akakehcn@gmail.com>
2025-02-27 14:46:54 +0900

    temp

--- src/main/java/org/scoula/backend/order/controller/ChartController.java
+++ src/main/java/org/scoula/backend/order/controller/ChartController.java
@@ -2,12 +2,8 @@ package org.scoula.backend.order.controller;
 
 import org.scoula.backend.order.dto.ChartResponseDto;
 import org.scoula.backend.order.service.TradeHistoryService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,17 +19,17 @@ public class ChartController {
 	private final TradeHistoryService tradeHistoryService;
 	private final SimpMessagingTemplate messagingTemplate;
 
-	// charthistory 초기 값 호출
-	@GetMapping("/{symbol}/history")
-	public ResponseEntity<ChartResponseDto> getChartHistory(@PathVariable("symbol") String symbol) {
-		try {
-			ChartResponseDto response = tradeHistoryService.getChartHistory(symbol);
-			return ResponseEntity.ok(response);
-		} catch (Exception e) {
-			log.error("Failed to get chart history for symbol: {}", symbol, e);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
+	// // charthistory 초기 값 호출
+	// @GetMapping("/{symbol}/history")
+	// public ResponseEntity<ChartResponseDto> getChartHistory(@PathVariable("symbol") String symbol) {
+	// 	try {
+	// 		ChartResponseDto response = tradeHistoryService.getChartHistory(symbol);
+	// 		return ResponseEntity.ok(response);
+	// 	} catch (Exception e) {
+	// 		log.error("Failed to get chart history for symbol: {}", symbol, e);
+	// 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	// 	}
+	// }
 
 	// 차트 웹소켓
 	@Scheduled(fixedRate = 15000) // 15초마다 새로운 캔들 생성
